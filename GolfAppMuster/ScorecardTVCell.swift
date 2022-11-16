04802cbcb21b053290d1203dd8efca3be3bc2dca
PatrickJahn91 <patrickjahn01@googlemail.com>
2022-11-16 14:28:04 +0100

    16.11.22

---
+++ GolfAppMuster/ScorecardTVCell.swift
@@ -0,0 +1,51 @@
+//
+//  ScorecardTVCell.swift
+//  GolfAppMuster
+//
+//  Created by Patrick Jahn on 15.11.22.
+//
+
+import UIKit
+
+class ScorecardTVCell: UITableViewCell {
+    
+    @IBOutlet weak var bahnnummerLabel: UILabel!
+    @IBOutlet weak var parLabel: UILabel!
+    @IBOutlet weak var hcpLabel: UILabel!
+    @IBOutlet weak var vorgabeHeaderLabel: UILabel!
+    @IBOutlet weak var shotsHeaderLabel: UILabel!
+    @IBOutlet weak var distanceWhiteLabel: UILabel!
+    @IBOutlet weak var distanceYellowLabel: UILabel!
+    @IBOutlet weak var distanceRedLabel: UILabel!
+    @IBOutlet weak var distanceBlackLabel: UILabel!
+    @IBOutlet weak var distanceOrangeLabel: UILabel!
+    @IBOutlet weak var finishHoleButton: UIButton!
+    
+    @IBOutlet weak var vorgabeP1Label: UILabel!
+    @IBOutlet weak var shotsP1Textfield: UITextField!
+    
+    
+    @IBOutlet weak var vorgabeP2Label: UILabel!
+    @IBOutlet weak var shotsP2Textfield: UITextField!
+    
+    
+    @IBOutlet weak var vorgabeP3Label: UILabel!
+    @IBOutlet weak var shotsP3Textfield: UITextField!
+    
+    
+    @IBOutlet weak var vorgabeP4Label: UILabel!
+    @IBOutlet weak var shotsP4Textfield: UITextField!
+
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+
+}
--- MatchViewController.swift
+++ MatchViewController.swift
@@ -12,8 +12,39 @@ import UIKit
 class MatchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
     @IBOutlet weak var ScoreCardTV: UITableView!
     
+    @IBOutlet weak var endRoundButton: UIButton!
     
-
+    
+    
+    @IBOutlet weak var NameP1: UITextField!
+    @IBOutlet weak var handicapP1: UITextField!
+    @IBOutlet weak var teeP1Button: UIButton!
+    
+    
+    @IBOutlet weak var nameP2: UITextField!
+    @IBOutlet weak var handicapP2: UITextField!
+    @IBOutlet weak var teeP2Button: UIButton!
+    
+    
+    
+    @IBOutlet weak var nameP3: UITextField!
+    @IBOutlet weak var handicapP3: UITextField!
+    @IBOutlet weak var teeP3Button: UIButton!
+    
+    
+    
+    @IBOutlet weak var nameP4: UITextField!
+    @IBOutlet weak var handicapP4: UITextField!
+    @IBOutlet weak var teeP4Button: UIButton!
+    
+    
+    
+    @IBOutlet weak var startRoundButton: UIButton!
+    @IBOutlet weak var showFrontNineButton: UIButton!
+    @IBOutlet weak var showBackNineButton: UIButton!
+    
+    @IBOutlet weak var showOther: UIButton!
+    
     
 
     override func viewDidLoad() {
@@ -21,24 +52,70 @@ class MatchViewController: UIViewController, UITableViewDataSource, UITableViewD
 
         ScoreCardTV.delegate = self
         ScoreCardTV.dataSource = self
+        
+        let teeOptions = {
+        (action: UIAction) in
+            print(action.title)
+        }
+        
+        teeP1Button.menu = UIMenu(children: [
+            UIAction(title: "H, weiß", state: .on, handler: teeOptions),
+            UIAction(title: "H, gelb", state: .on, handler: teeOptions),
+            UIAction(title: "H, rot", state: .on, handler: teeOptions),
+            UIAction(title: "D, schwarz", state: .on, handler: teeOptions),
+            UIAction(title: "D, rot", state: .on, handler: teeOptions),
+            UIAction(title: "D, orange", state: .on, handler: teeOptions)
+        ]
+        )
+        
+        teeP2Button.menu = UIMenu(children: [
+            UIAction(title: "H, weiß", state: .on, handler: teeOptions),
+            UIAction(title: "H, gelb", state: .on, handler: teeOptions),
+            UIAction(title: "H, rot", state: .on, handler: teeOptions),
+            UIAction(title: "D, schwarz", state: .on, handler: teeOptions),
+            UIAction(title: "D, rot", state: .on, handler: teeOptions),
+            UIAction(title: "D, orange", state: .on, handler: teeOptions)
+        ]
+        )
+        
+        teeP3Button.menu = UIMenu(children: [
+            UIAction(title: "H, weiß", state: .on, handler: teeOptions),
+            UIAction(title: "H, gelb", state: .on, handler: teeOptions),
+            UIAction(title: "H, rot", state: .on, handler: teeOptions),
+            UIAction(title: "D, schwarz", state: .on, handler: teeOptions),
+            UIAction(title: "D, rot", state: .on, handler: teeOptions),
+            UIAction(title: "D, orange", state: .on, handler: teeOptions)
+        ]
+        )
+        
+        teeP4Button.menu = UIMenu(children: [
+            UIAction(title: "H, weiß", state: .on, handler: teeOptions),
+            UIAction(title: "H, gelb", state: .on, handler: teeOptions),
+            UIAction(title: "H, rot", state: .on, handler: teeOptions),
+            UIAction(title: "D, schwarz", state: .on, handler: teeOptions),
+            UIAction(title: "D, rot", state: .on, handler: teeOptions),
+            UIAction(title: "D, orange", state: .on, handler: teeOptions)
+        ]
+        )
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 200
+        return 150
         
         //returnwert ist Höhe einer Zeile
     }
     
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 9
+        return 18
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         //TODO: Custom Tableview Cell Klasse einrichten
         
-        var cell = ScoreCardTV.dequeueReusableCell(withIdentifier: "scorecardLine", for: indexPath)
+        var cell = ScoreCardTV.dequeueReusableCell(withIdentifier: "scorecardLine", for: indexPath) as! ScorecardTVCell
+        cell.bahnnummerLabel.text = "Bahn \(indexPath.row+1)"
         return cell
     }
 
