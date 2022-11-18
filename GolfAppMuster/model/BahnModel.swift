4b7596610329b7e6c37de6756a11c80c6441fc17
PatrickJahn91 <patrickjahn01@googlemail.com>
2022-11-18 16:18:15 +0100

    18.11.2022

---
+++ GolfAppMuster/model/BahnModel.swift
@@ -0,0 +1,23 @@
+//
+//  bahnModel.swift
+//  GolfAppMuster
+//
+//  Created by Patrick Jahn on 16.11.22.
+//
+
+import Foundation
+
+
+struct Bahn {
+    var bahnnummer : Int
+    var bahnPar : Int
+    var bahnHcp : Int
+    
+    var distanceWeiss : Int
+    var distanceGelb : Int
+    var distanceSchwarz : Int
+    var distanceRot : Int
+    var distanceOrange : Int
+    
+    
+}
